24b702f90af25a0f8f249945d85811dc74877d12
hypothe <markeagle98@gmail.com>
2020-12-05 10:40:41 +0000

    periodic server

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <unistd.h>
+#include <sys/select.h>
 
 
 #define MAX_HEIGHT 200
@@ -27,12 +28,15 @@ void error(char *msg, int ret)
 
 int main(int argc, char *argv[])
 {
+	char STATUS = 'B';
 	int sockfd, newsockfd, portno, clilen;
 	struct sockaddr_in serv_addr, cli_addr;
 	int ret;
+	unsigned long sec = 1, usec = 0;
 	char in=0;
-	// char buffer[BUFSIZE];
+	struct timeval tv;
 	msg_t msg = {0,0};
+	fd_set fd_in;
 
 
 	if (argc < 2) {
@@ -58,33 +62,58 @@ int main(int argc, char *argv[])
 	  error("ERROR on accept", newsockfd);
 
 	while(in!='E'){
-		if ((ret = read(newsockfd,&in,1)) < 0) error("ERROR reading from socket", ret);
-		printf("Received request ");
-		switch (in){
+		// --- preconfig ---
+		FD_ZERO(&fd_in);
+		FD_SET(newsockfd, &fd_in);
+		tv.tv_sec = sec; tv.tv_usec = usec;
+		in = 0;	// reset each time, in order to avoid problems if we somehow read 0Byte from the readable socket
+		
+		if((ret=select(newsockfd+1, &fd_in, NULL, NULL, &tv)) < 0){	error("Select on socket", ret); }
+		/* Updates status only if a new one is submitted and it is compatible with current status, otherwise stop the hoist */
+		if (FD_ISSET(newsockfd, &fd_in)){
+			if ((ret = read(newsockfd,&in,1)) < 0) error("ERROR reading from socket", ret);
+			printf("Received request: ");	// won't print nicely if we read 0 bit
+			if ( (in == 'U' && STATUS != 'T') || (in == 'D' && STATUS != 'B') || (in == 'E'))
+				STATUS = in;
+		}
+		
+		switch (STATUS){
 			case 'U':	 // go UP
-				if (msg.status == 'T') break;
-				msg.height += STEP*(1+rand()%(MAX_STEP-msg.height/STEP));
-				msg.status = msg.height>=MAX_HEIGHT?'T':'U';
-				printf("UP\n");
+				msg.height += STEP;
+				STATUS = msg.height>=MAX_HEIGHT?'T':'U';
+				msg.status = STATUS;
+				printf("UP: %d\n", msg.height);
+				break;
+			
+			case 'D': 	// go DOWN
+				msg.height -= STEP;
+				STATUS = msg.height<=0?'B':'D';
+				msg.status = STATUS;
+				printf("DOWN: %d\n", msg.height);
 				break;
 			case 'S': 	// STOP
-				msg.status = 'S';
-				printf("STOP\n");
+				printf("STOP: %d\n", msg.height);
+				msg.status = STATUS;
 				break;
-			case 'D': 	// go DOWN
-				if (msg.status == 'B') break;
-				msg.height -= STEP*(1+rand()%(msg.height/STEP));
-				msg.status = msg.height<=0?'B':'D';
-				printf("DOWN\n");
+			case 'T':
+				printf("TOP: %d\n", msg.height);
+				msg.status = STATUS;
+				break;
+			case 'B':
+				printf("BOTTOM: %d\n", msg.height);
+				msg.status = STATUS;
 				break;
 			case 'E':	// END
 				msg.status = 'E';
-				printf("END\n");
+				printf("END: %d\n", msg.height);
 				break;
-			// default 
+			default: // will simply do nothing
+				msg.status = STATUS;
 		}
-		if ((ret = write(newsockfd,&msg,sizeof(msg)))<0) error("ERROR writing on socket", ret); 
-	}
+		if ((ret = write(newsockfd,&msg,sizeof(msg)))<0) error("ERROR writing on socket", ret);
+		usleep(1000000*(sec-tv.tv_sec)+(usec-tv.tv_usec));	// tv will yield the amount of time passed before reading
+	}														// with this usleep we approximate a period of 1s for the serve
+															
 	close(newsockfd);
 	return 0; 
 }
