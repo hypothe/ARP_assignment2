804b75a263a65212b9e88173c7fe8ef2d31ce0a3
hypothe <markeagle98@gmail.com>
2020-12-06 20:27:00 +0000

    library template

---
+++ hoistlib.c
@@ -0,0 +1,1 @@
+#include "hoistlib.h"
---
+++ hoistlib.h
@@ -0,0 +1,13 @@
+#ifndef _HOISTLIB_H_
+#define _HOISTLIB_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+
+
+
+#endif
