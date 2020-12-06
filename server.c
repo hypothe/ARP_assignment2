ab57fa76c69b2abce754a5c15a70c857dad9e2e8
hypothe <markeagle98@gmail.com>
2020-12-06 10:35:14 +0000

    server and hoist communication

--- server.c
+++ server.c
@@ -11,9 +11,13 @@
 
 
 #define MAX_HEIGHT 200
+#define MIN_HEIGHT 0
 #define STEP 5
 #define MAX_STEP MAX_HEIGHT/STEP
 #define BUFSIZE 20
+
+
+int spawn(char *ex_name, int *fd_in, int *fd_out, char new_shell);
 
 typedef struct message{
 	int height;
@@ -37,14 +41,27 @@ int main(int argc, char *argv[])
 	struct timeval tv;
 	msg_t msg = {0,0};
 	fd_set fd_in;
-
+	int s2h[2], h2s[2]; // pipes for server - hoist communication
+	pid_t hoist;
+	char send=0;
+	int r_height=0;
 
 	if (argc < 2) {
-	 fprintf(stderr,"ERROR, no port provided\n");
-	 exit(1);
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
 	}
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-	error("ERROR opening socket", sockfd);
+		error("ERROR opening socket", sockfd);
+		
+	if((ret = pipe(s2h))<0){
+		error("ERROR opening Server - Hoist pipe", ret);
+	}	
+	if((ret = pipe(h2s))<0){
+		error("ERROR opening Hoist - Server pipe", ret);
+	}
+	/* s2h is the input pipe of the hoist, h2s is its output pipe */
+	/* Spawn without opening a new shell, since hoist doesn't output anything */
+	if((hoist = spawn("./hoist", s2h, h2s, 0))<0) error("ERROR hoist generation", hoist);
 
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	portno = atoi(argv[1]);
@@ -59,7 +76,7 @@ int main(int argc, char *argv[])
 	clilen = sizeof(cli_addr);
 
 	if ((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) < 0) 
-	  error("ERROR on accept", newsockfd);
+		error("ERROR on accept", newsockfd);
 
 	while(in!='E'){
 		// --- preconfig ---
@@ -72,48 +89,103 @@ int main(int argc, char *argv[])
 		/* Updates status only if a new one is submitted and it is compatible with current status, otherwise stop the hoist */
 		if (FD_ISSET(newsockfd, &fd_in)){
 			if ((ret = read(newsockfd,&in,1)) < 0) error("ERROR reading from socket", ret);
-			printf("Received request: ");	// won't print nicely if we read 0 bit
-			if ( (in == 'U' && STATUS != 'T') || (in == 'D' && STATUS != 'B') || (in == 'E') || (in == 'S'))
+			printf("Received request: ");	fflush(stdout);// won't print nicely if we read 0 bit
+			if ((in == 'U' && STATUS != 'T') || (in == 'D' && STATUS != 'B') || (in == 'E') || (in == 'S'))
 				STATUS = in;
 		}
 		
 		switch (STATUS){
 			case 'U':	 // go UP
-				msg.height += STEP;
-				STATUS = msg.height>=MAX_HEIGHT?'T':'U';
-				msg.status = STATUS;
-				printf("UP: %d\n", msg.height);
+				send = '+';
+				// msg.height += STEP;
+				// STATUS = 'U';
+				// STATUS = msg.height>=MAX_HEIGHT?'T':'U';
+				// msg.status = STATUS;
+				// printf("UP: %d\n", msg.height);
 				break;
-			
 			case 'D': 	// go DOWN
-				msg.height -= STEP;
-				STATUS = msg.height<=0?'B':'D';
-				msg.status = STATUS;
-				printf("DOWN: %d\n", msg.height);
+				send = '-';
+				// msg.height -= STEP;
+				// STATUS = msg.height<=0?'B':'D';
+				// msg.status = STATUS;
+				// printf("DOWN: %d\n", msg.height);
 				break;
 			case 'S': 	// STOP
-				printf("STOP: %d\n", msg.height);
-				msg.status = STATUS;
+				send = 0;
+				// printf("STOP: %d\n", msg.height);
+				// msg.status = STATUS;
 				break;
 			case 'T':
-				printf("TOP: %d\n", msg.height);
-				msg.status = STATUS;
+				send = 0;
+				// printf("TOP: %d\n", msg.height);
+				// msg.status = STATUS;
 				break;
 			case 'B':
-				printf("BOTTOM: %d\n", msg.height);
-				msg.status = STATUS;
+				send = 0;
+				// printf("BOTTOM: %d\n", msg.height);
+				// msg.status = STATUS;
 				break;
 			case 'E':	// END
-				msg.status = 'E';
-				printf("END: %d\n", msg.height);
+				send = 'E';
+				// msg.status = 'E';
+				// printf("END: %d\n", msg.height);
 				break;
-			default: // will simply do nothing
-				msg.status = STATUS;
+			//default: // will simply do nothing
+				// msg.status = STATUS;
 		}
+		if ((ret = write(s2h[1],&send,1))<0) 			error("ERROR writing on Server - Hoist pipe", ret);
+		/* MAYBE implement some sort of timeout, so the server will know if the hoist crashed */
+		if ((ret = read(h2s[0],&r_height,sizeof(r_height)))<0) 	error("ERROR reading from Hoist - Server pipe", ret);
+		if (r_height>=MAX_HEIGHT) STATUS = 'T';	// set TOP or BOTTOM state
+		if (r_height<=MIN_HEIGHT) STATUS = 'B';	// the condition allows to take the more strict height limit if
+												// the Server and Hoist one are different (this is not the case)
+		msg.height = r_height; msg.status = STATUS;
+		printf("%c %d\n", msg.status, msg.height);	// FOR DEBUGGING
 		if ((ret = write(newsockfd,&msg,sizeof(msg)))<0) error("ERROR writing on socket", ret);
 		usleep(1000000*(sec-tv.tv_sec)+(usec-tv.tv_usec));	// tv will yield the amount of time passed before reading
 	}														// with this usleep we approximate a period of 1s for the serve
-															
+	
+	close(s2h[1]);
+	close(h2s[0]);
 	close(newsockfd);
 	return 0; 
+}
+
+int spawn(char* ex_name, int *fd_in, int *fd_out, char new_shell) {
+	int ret;
+	pid_t child_pid = fork();
+	char *args[6];
+	int i = 0;
+	if (child_pid != 0)
+	{	
+		/* Father closes fd_in[0], since it will write on it */
+		/* Father closes fd_out[1], since it will read from it */
+		if (fd_in!=NULL) 	close(fd_in[0]);
+		if (fd_out!=NULL)	close(fd_out[1]);
+		return child_pid;
+	}
+	else 
+	{
+		/* Child closes fd_in[1], since it will read from it */
+		/* Father closes fd_out[0], since it will write on it */
+		if (fd_in!=NULL)	close(fd_in[1]);
+		if (fd_out!=NULL)	close(fd_out[0]);
+		char tmp_in[5]="", tmp_out[5]="";
+		sprintf(tmp_in, "%d", fd_in[0]);
+		sprintf(tmp_out, "%d", fd_out[1]);
+		if (new_shell){ 
+		//char *args[] = { "/usr/bin/konsole",  "-e", ex_name, tmp_in, tmp_out, (char*)NULL }; 
+		args[i++] = "/usr/bin/konsole";
+		args[i++] = "-e";
+		}
+		//char *args[] = { ex_name, tmp_in, tmp_out, (char*)NULL };
+		args[i++] = ex_name;
+		args[i++] = tmp_in;
+		args[i++] = tmp_out;
+		args[i++] = (char*)NULL;
+		
+		ret = execvp(args[0], args);
+		perror("exec failed");
+		exit(ret);
+	}
 }
